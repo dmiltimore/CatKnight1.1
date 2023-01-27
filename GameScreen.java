673a4c08ad038c92cf47b59a8967766a30d5c00f
dmiltimore <daltonmiltimore@gmail.com>
2023-01-27 15:41:31 -0800

    added fields to GameScreen

---
+++ GameScreen.java
@@ -0,0 +1,6 @@
+public class GameScreen {
+    private int tileSize;
+    private int scaleFactor = 3; //to scale sprites so they ain't tiny
+    private int maxScreenCol = 12;
+    private int maxSceenRow = 16;
+}
