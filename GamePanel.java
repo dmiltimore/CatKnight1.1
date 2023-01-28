350d51fb61d0078ee088193f3e942820cf9f6b3d
dmiltimore <daltonmiltimore@gmail.com>
2023-01-28 12:06:53 -0800

    Changed name of GameScreen to GamePanel

--- GamePanel.java
+++ GamePanel.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import javax.swing.*;
 
-public class GameScreen {
+public class GamePanel {
     private int tileSize;
     private int scaleFactor = 3; //to scale sprites and screen so they ain't tiny
     private int maxScreenCol = 12;
