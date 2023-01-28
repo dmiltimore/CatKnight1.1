42ed20e79874a7a8f2ddd16c0cf10477b33a05f2
evanttrane <evanttran@gmail.com>
2023-01-28 12:15:58 -0800

    main

--- Main/Main.java
+++ Main/Main.java
@@ -1,3 +1,23 @@
-public class Main {
-    
+package Main;
+
+import javax.swing.JFrame;
+
+public class MainClass {
+    public static void main(String[] theArgs){
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Practice is lit"); 
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+    }
+
+
 }
