3743030426c0f523a2a24c1e2b087c63994fb1ad
dmiltimore <daltonmiltimore@gmail.com>
2023-01-28 12:33:21 -0800

    indenting

--- Entity/Entity.java
+++ Entity/Entity.java
@@ -7,50 +7,48 @@ public class Entity {
     private int myDef;
     private int mySpd;
     private int myMoveSpd;
-    
-}
-
-//getters
-public int getX() {
-    return myX;
-}
-public int getY() {
-    return myY;
-}
-public int getHP() {
-    return myHP;
-}
-public int getAtk() {
-    return myAtk;
-}
-public int getDef() {
-    return myDef;
-}
-public int getSpd() {
-    return mySpd;
-}
-public int getMoveSpd() {
-    return myMoveSpd;
-}
-//setters
-public void setX(int theX) {
-    myX = theX;
-}
-public void setY(int theY) {
-    myY = theY;
-}
-public void setHP(int theHP) {
-    myHP = theHP;
-}
-public void setAtk(int theAtk) {
-    myAtk = theAtk;
-}
-public void setDef(int theDef) {
-    myDef = theDef;
-}
-public void setSpd(int theSpd) {
-    mySpd = theSpd;
-}
-public void setSpd(int theMoveSpd) {
-    myMoveSpd = theMoveSpd;
+    //getters
+    public int getX() {
+        return myX;
+    }
+    public int getY() {
+        return myY;
+    }
+    public int getHP() {
+        return myHP;
+    }
+    public int getAtk() {
+        return myAtk;
+    }
+    public int getDef() {
+        return myDef;
+    }
+    public int getSpd() {
+        return mySpd;
+    }
+    public int getMoveSpd() {
+        return myMoveSpd;
+    }
+    //setters
+    public void setX(int theX) {
+        myX = theX;
+    }
+    public void setY(int theY) {
+        myY = theY;
+    }
+    public void setHP(int theHP) {
+        myHP = theHP;
+    }
+    public void setAtk(int theAtk) {
+        myAtk = theAtk;
+    }
+    public void setDef(int theDef) {
+        myDef = theDef;
+    }
+    public void setSpd(int theSpd) {
+        mySpd = theSpd;
+    }
+    public void setSpd(int theMoveSpd) {
+        myMoveSpd = theMoveSpd;
+    }
 }
