142076c59336e7a5e9c739ff5e32423714f0e5c9
evanttrane <evanttran@gmail.com>
2023-01-28 14:55:09 -0800

    added import statements to player class

--- Entity/Player.java
+++ Entity/Player.java
@@ -1,6 +1,9 @@
 package Entity;
 import Main.GamePanel;
 import Main.KeyHandler;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
 
 public class Player extends Entity {
 
@@ -14,24 +17,25 @@ public class Player extends Entity {
     public void setDefaultValues() {
         myX = 100;
         myY = 100;
-        setMoveSpd(4);
+        super.setMoveSpd(4);
     }
     public void update() {
         if(keyH.upPressed == true){
-            myY -= getMoveSpd();
+            myY -= super.getMoveSpd();
         }
         else if(keyH.downPressed == true){
-            myY += getMoveSpd();
+            myY += super.getMoveSpd();
         }
         else if(keyH.leftPressed == true){
-            myX -= getMoveSpd();
+            myX -= super.getMoveSpd();
 
         }
         else if(keyH.rightPressed == true){
-            myX += getMoveSpd();
+            myX += super.getMoveSpd();
     }
     public void draw(Graphics2D g2) {
         g2.setColor(Color.white);
         g2.fillRect(myX, myY, gp.getTileSize(), gp.getTileSize());
     }
+}
 }
