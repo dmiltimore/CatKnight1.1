888c2efc34f8fb0778b5e9eccfd6947144181d3b
Jayana Estacio <jpstacio@uw.edu>
2023-01-27 20:00:34 -0800

    updated items class

--- Characters.java
+++ Characters.java
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-public class Characters {
-    public void 
-=======
 public class Characters extends Entity {
     
->>>>>>> 2a4cce36a5d65fca1ca2f04bdc4f528fa4fc4328
 }
--- Items.java
+++ Items.java
@@ -1,3 +1,21 @@
-public class Items {
-    
+public abstract class Items implements Description{
+    //fields
+    private String NAME;
+    private String DESCRIPTION;
+
+    // passes name and description of item
+    public Items(String name, String description) {
+        this.NAME = name;
+        this.DESCRIPTION = description;
+    }
+
+    // returns items name
+    public String getNAME() {
+        return this.NAME;
+    }
+
+    // returns iteams description
+    public String readDescription() {
+        return this.DESCRIPTION;
+    }
 }
