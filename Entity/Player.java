08dc3631f6396b5573dc930302953e595caf7d63
dmiltimore <daltonmiltimore@gmail.com>
2023-01-28 14:07:42 -0800

    Woot

--- Entity/Player.java
+++ Entity/Player.java
@@ -10,12 +10,4 @@ public class Player extends Entity {
     GamePanel gp;
     KeyHandler keyH;
 
-    public Player(GamePanel gp, KeyHandler theKeyH) {
-        this.gp = gp;
-        this.keyH = theKeyH;
-    }
-    public void setDefaultValues() {
-        
-    }
-
 }
