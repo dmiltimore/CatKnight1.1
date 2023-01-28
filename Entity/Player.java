978e9f532fecf5ea6d43d917ec516a5795901271
dmiltimore <daltonmiltimore@gmail.com>
2023-01-28 14:20:30 -0800

    Player class

--- Entity/Player.java
+++ Entity/Player.java
@@ -4,14 +4,19 @@ import main.GamePanel;
 import main.KeyHandler;
 
 import Main.GamePanel;
+import Main.KeyHandler;
 public class Player extends Entity {
 
     GamePanel gp;
     KeyHandler keyH;
 
     public Player(GamePanel gp, KeyHandler keyH){
-        
         this.gp = gp;
         this.keyH = keyH;
+    }
+    public void setDefaultValues() {
+        myX = 100;
+        myY = 100;
+        setMoveSpd(4);
     }
 }
