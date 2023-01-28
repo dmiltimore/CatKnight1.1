fd6ce3d06eb673546d76750afb285a0ec66c430e
dmiltimore <daltonmiltimore@gmail.com>
2023-01-28 12:27:33 -0800

    private fields

--- Main/GamePanel.java
+++ Main/GamePanel.java
@@ -7,25 +7,14 @@ import javax.swing.JPanel;
 public class GamePanel extends JPanel{
 
     //Screeen Settings
-<<<<<<< HEAD
-    final  int originalTileSize = 16; //16 x 16 tile
-    final int scale = 3;
-    
-    final int tileSize = originalTileSize * scale; //48 x 48
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol; //768 pixels
-    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
-=======
-    final private int originalTileSize = 16;
+    final private int originalTileSize = 16; //16 x 16 tile
     final private int scale = 3;
     
-    final private int tileSize = originalTileSize * scale;
+    final private int tileSize = originalTileSize * scale; //48 x 48
     final private int maxScreenCol = 16;
     final private int maxScreenRow = 12;
-    final private int screenWidth = tileSize * maxScreenCol;
-    final private int screenHeight = tileSize * maxScreenRow;
->>>>>>> a433d3cb887822a4a9b5924f1c89f554c939ca7d
+    final private int screenWidth = tileSize * maxScreenCol; //768 pixels
+    final private int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
     public GamePanel() {
 
