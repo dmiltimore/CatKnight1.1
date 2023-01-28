68f4f2e3759a90d898df711ba52716e168fc5227
evanttrane <evanttran@gmail.com>
2023-01-28 12:14:53 -0800

    game panel changes

--- Main/GamePanel.java
+++ Main/GamePanel.java
@@ -1,10 +1,28 @@
 package Main;
-import java.io.*;
-import javax.swing.*;
 
-public class GamePanel {
-    private int tileSize;
-    private int scaleFactor = 3; //to scale sprites and screen so they ain't tiny
-    private int maxScreenCol = 12;
-    private int maxSceenRow = 16;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+
+    //Screeen Settings
+    final  int originalTileSize = 16;
+    final int scale = 3;
+    
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));    
+        this.setBackground(Color.black);    
+        this.setDoubleBuffered(true);
+
+
+    }
+    
 }
