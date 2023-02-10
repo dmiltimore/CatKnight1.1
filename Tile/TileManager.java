92f6ebb9e0f5ee2055518298241f7bfaa082e4b0
Jayana Estacio <jpstacio@uw.edu>
2023-02-10 14:58:42 -0800

    merge fix

--- Tile/TileManager.java
+++ Tile/TileManager.java
@@ -40,16 +40,13 @@ public class TileManager {
             tile[2].image = ImageIO.read(getClass().getResourceAsStream("/Assets/Tiles/tree.png"));
 
             tile[3] = new Tile();
-<<<<<<< HEAD
             tile[3].image = ImageIO.read(getClass().getResourceAsStream("/Assets/Tiles/dirt.png"));
-=======
-            tile[3].image = ImageIO.read(getClass().getResource("/Assets/Tiles/dirt.png"));
 
             tile[4] = new Tile();
             tile[4].image = ImageIO.read(getClass().getResource("/Assets/Tiles/sand.png"));
 
             
->>>>>>> 3d7c80503ae1ae876ce4633c7577277d93910f5d
+
         } catch(IOException e) {
             e.printStackTrace();
         }
