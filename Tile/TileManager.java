667c8b66d6792b655a7d1301c07363e72fdd51f4
dmiltimore <daltonmiltimore@gmail.com>
2023-02-10 15:32:47 -0800

    changed file path

--- Tile/TileManager.java
+++ Tile/TileManager.java
@@ -25,7 +25,7 @@ public class TileManager {
         mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
 
         getTileImage();
-        loadMap("/Assets/Maps/StageTown.txt"); // Path to Map
+        loadMap("/Assets/Maps/maps/StageTown.txt"); // Path to Map
 
     }
     public void getTileImage() {
